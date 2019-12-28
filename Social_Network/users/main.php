e1663872356e000934f7cedfff80945d236892c6
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2019-12-28 21:17:32 +0600

    Add files via upload

---
+++ Social_Network/users/main.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Codingcafe</title>
+	  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<!-- For creating a simple background
+ -->
+ <div class="row">
+	<div class="col-sm-12">
+		<div class="well">
+			<center><h1>Coding Cafe</h1></center>
+		</div>
+	</div>
+</div>
+
+
+
+<!-- For adding  a image for backgournd
+ -->
+<div class="row">
+	<div class="col-sm-6" style="left: 0.5%;">
+		<!-- <img src="opt/lampp/htdocs/Social_Network/images/img1.jpg"
+		title="Coding experience"> -->
+		<!-- <img src="/opt/lampp/htdocs/Social_Network/users/codingcafe.jpeg" alt="codingcafe" class="img-rounded" title="Coding cafe" width="2880px" height="1800px"> -->
+
+		<a href="#"><img src="/opt/lampp/htdocs/Social_Network/users/img4.png" width="590px" height="100px" /></a>
+
+	</div>
+</div>
+
+
+
+</body>
+</html>
+ <!-- width="650px" height="565px" -->
